3754166a477c3d78615a90209341bf05de0121c7
twong0088 <terrencewong@annas-mbp.lan>
2022-10-20 22:03:53 -0700

    change server file for joey page

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +9,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
-app.use("/joey", express.static(__dirname + '/../joey/dist'))
+
+app.get('/joey', function (req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname, "/../joey/dist")});
+});
+
+app.get('/joey/:assetName', function(req, res) {
+  res.sendFile(req.params.assetName, {root: path.join(__dirname, "/../joey/dist")});
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
